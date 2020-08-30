e59167f64df26ee8ac9426b9d185ec550931d643
Pape Singane DIENG <papesingane@yahoo.fr>
2020-08-30 20:51:53 +0200

    Add unit tests

---
+++ Tests/Unit/Common.Tests/ConfigurationHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NFluent;
+using Xunit;
+
+namespace Common.Tests
+{
+    public class ConfigurationHelperTests
+    {
+	    [Fact]
+	    public void Should_Get_IntValue()
+	    {
+		    var result = ConfigurationHelper.Get<int>("IntValue", 456);
+		    Check.That(result).Equals(123);
+	    }
+
+	    [Fact]
+	    public void Should_Get_DateValue()
+	    {
+			var expectedDate  = new DateTime(2020, 10, 5);
+		    var result = ConfigurationHelper.Get<DateTime>("DateValue");
+		    Check.That(result).Equals(expectedDate);
+	    }
+
+	    [Fact]
+	    public void Should_Get_Default_Value()
+	    {
+		    var result = ConfigurationHelper.Get("UndefinedKey", 123);
+		    Check.That(result).Equals(123);
+	    }
+
+	    [Fact]
+	    public void Should_Throw_Exception_When_Undefined_Key_And_No_Default_Value_Set()
+	    {
+		    Check.ThatCode(() => ConfigurationHelper.Get<string>("UndefinedKey"))
+			    .Throws<Exception>()
+			    .WithMessage("Configuration Exception: unable to find key 'UndefinedKey'");
+	    }
+	}
+}
